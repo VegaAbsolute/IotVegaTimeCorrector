a4e1d3565a686e1a00ec351c0c2a2906755a49bc
alk120ger <alk120ger@yandex.ru>
2018-12-26 13:24:10 +0700

    code refactoring

--- libs/config.js
+++ libs/config.js
@@ -1,77 +1,79 @@
+//config.ini version 1.0.0
+let moment = require('moment');
 class Config
 {
-  constructor()
+  constructor ()
   {
-    this._ws={
-      url:'ws://0.0.0.0:8002',
-      login:'root',
-      password:'123'
+    this._ws = {
+      url: 'ws://0.0.0.0:8002',
+      login: 'root',
+      password: '123'
     };
     this._system = {
-      settings:{
-        auto_update:false
+      settings: {
+        auto_update: false
       }
     };
     this._debugMOD = {
-      status:false,
-      settings:{}
+      status: false,
+      settings: {}
     };
   }
   //setters-------------------------
-  set ws_user(val)
+  set ws_user ( val )
   {
     this._ws.login = val;
   }
-  set ws_password(val)
+  set ws_password ( val )
   {
     this._ws.password = val;
   }
-  set ws_address(val)
+  set ws_address ( val )
   {
     this._ws.url = val;
   }
-  set other_debug_enabled(val)
+  set other_debug_enabled ( val )
   {
     this._debugMOD.status = val;
   }
   //getters-------------------------
-  get ws()
+  get ws ()
   {
     return this._ws.url;
   }
-  get loginWS()
+  get loginWS ()
   {
     return this._ws.login.toString();
   }
-  get passwordWS()
+  get passwordWS ()
   {
     return this._ws.password.toString();
   }
-  get debugMOD()
+  get debugMOD ()
   {
     return this._debugMOD.status;
   }
   //methods
-  setFromConfig(config)
+  setFromConfig ( config )
   {
-    for (let section in config)
+    for ( let section in config )
     {
       try
       {
-        for(let param in config[section])
+        for( let param in config[section] )
         {
           this[section+'_'+param] = config[section][param];
         }
       }
-      catch (e)
+      catch ( e )
       {
-        console.error(e);
+        console.error( moment().format('LLL'), e );
         return false;
       }
     }
     return true;
   }
-  valid()
+  valid ()
   {
     return true;
   }
