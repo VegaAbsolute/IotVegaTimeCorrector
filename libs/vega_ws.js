d30c4bcead78dfb82d5622b2fc2236fb1f784cbf
alk120ger <alk120ger@yandex.ru>
2018-12-26 13:51:19 +0700

    code refactoring

--- libs/vega_ws.js
+++ libs/vega_ws.js
@@ -1,6 +1,11 @@
-const WebSocket = require('ws');
-const EventEmitter = require('events');
-const moment = require('moment');
+//vega_ws.ini version 1.0.0
+const DELAY_MESSAGE = 10000;
+const DELAY_RECONNECT = 5000;
+
+let WebSocket = require( 'ws' );
+let EventEmitter = require( 'events' );
+let moment = require( 'moment' );
+
 class VegaWS extends EventEmitter
 {
   constructor(url)
@@ -8,95 +13,95 @@ class VegaWS extends EventEmitter
     super();
     this.url = url;
     this._connect = {
-      _status:false
+      _status: false
     };
     this.reload();
-    const intervel = setInterval(()=>{
-      if(!this.status)
+    const intervel = setInterval( ()=>{
+      if( !this.status )
       {
-        var currentDate = new Date().getTime();
-        var validLastTimeReconnect = this.last_time_reconnect!==false;
-        var lastDate = validLastTimeReconnect?this.last_time_reconnect:currentDate;
-        var time = currentDate-lastDate;
-        if(time>10000)
+        let currentDate = new Date().getTime();
+        let validLastTimeReconnect = this.last_time_reconnect !== false;
+        let lastDate = validLastTimeReconnect ? this.last_time_reconnect : currentDate;
+        let time = currentDate - lastDate;
+        if( time > DELAY_MESSAGE )
         {
           this.reload();
         }
       }
-    }, 5000);
+    }, DELAY_RECONNECT );
   }
-  get last_time_reconnect()
+  get last_time_reconnect ()
   {
-    var validLastTimeReconnect = this._connect._last_time_reconnect!==undefined&&typeof this._connect._last_time_reconnect==='number';
-    return validLastTimeReconnect?this._connect._last_time_reconnect:false;
+    let validLastTimeReconnect = this._connect._last_time_reconnect !== undefined && typeof this._connect._last_time_reconnect === 'number';
+    return validLastTimeReconnect ? this._connect._last_time_reconnect : false;
   }
-  set last_time_reconnect(ltr)
+  set last_time_reconnect ( ltr )
   {
     this._connect._last_time_reconnect = new Date().getTime();
   }
-  get status()
+  get status ()
   {
     return this._connect._status;
   }
-  set status(st)
+  set status ( st )
   {
     this._connect._status = st;
   }
-  reload()
+  reload ()
   {
-    this._connect = new WebSocket(this.url);
+    this._connect = new WebSocket( this.url );
     this.status = false;
     this._connect._self = this;
     this.last_time_reconnect = new Date().getTime();
-    this._connect.on('open',this._open);
-    this._connect.on('message',this._message);
-    this._connect.on('error',this._error);
-    this._connect.on('close',this._close);
+    this._connect.on('open', this._open);
+    this._connect.on('message', this._message);
+    this._connect.on('error', this._error);
+    this._connect.on('close', this._close);
   }
-  _message(message)
+  _message ( message )
   {
     try
     {
-      let obj = JSON.parse(message);
-      let validCMD = obj.cmd!=undefined;
-      if(validCMD)
+      let obj = JSON.parse( message );
+      let validCMD = obj.cmd != undefined;
+      if ( validCMD )
       {
-        this._self.emit(obj.cmd,obj)
+        this._self.emit ( obj.cmd, obj )
       }
     }
-    catch (e)
+    catch ( e )
     {
-      console.log(moment().format('LLL')+': ',e);
+      console.log( moment().format('LLL') + ': ', e );
     }
     finally
     {
       return;
     }
   }
-  _error()
+  _error ()
   {
-    console.log(moment().format('LLL')+': ','WS error');
+    console.log( moment().format('LLL') + ': ', 'WS error');
     this._status = false;
-    this._self.emit('no_connect');
+    this._self.emit( 'no_connect' );
   }
-  _close(code)
+  _close ( code )
   {
-    console.log(moment().format('LLL')+': ','WS close');
+    console.log( moment().format('LLL') + ': ', 'WS close' );
     this._status = false;
-    this._self.emit('no_connect');
+    this._self.emit( 'no_connect' );
   }
-  _open()
+  _open ()
   {
-    console.log(moment().format('LLL')+': ','Successful connection on WS');
+    console.log( moment().format('LLL') + ': ', 'Successful connection on WS' );
     this._status = true;
-    this._self.emit('run')
+    this._self.emit( 'run' )
   }
-  send_json(obj)
+  send_json ( obj )
   {
-    this._connect.send(JSON.stringify(obj),function(e){
-      if(e)
+    this._connect.send( JSON.stringify( obj ), function( e ) {
+      if ( e )
       {
-        this._status=false;
+        this._status = false;
       }
     });
   }
